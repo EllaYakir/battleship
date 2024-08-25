3a4da29b0ad137d54db589ea0ff22b7470245997
Ella Yakir <ellayakir@mail.tau.ac.il>
2024-08-25 21:00:44 +0300

    initial commit

--- objects.py
+++ objects.py
@@ -1,3 +1,11 @@
+class Board:
+    def __init__(self):
+        self.layer0 = ...
+        self. layer1 = ...
+        self. layer2 = ...
+    pass
+
+
 class Submarine:
     pass
 
@@ -8,3 +16,11 @@ class Destroyer:
 
 class Jet:
     pass
+#%%
+import numpy as np
+from matplotlib import pyplot as plt
+
+random_image = np.random.random([500, 500])
+
+plt.imshow(random_image, cmap='gray')
+plt.colorbar()
