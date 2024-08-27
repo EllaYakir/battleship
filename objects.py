7a123dba5d110b36cab994ac504e5a42a81e1b13
Ella Yakir <ellayakir@mail.tau.ac.il>
2024-08-27 03:39:13 +0300

    update

--- objects.py
+++ objects.py
@@ -1,46 +1,66 @@
 import numpy as np
 import random
 
+
 class Board:
-    def __init__(self, shape=(5, 5)):
+    def __init__(self, shape=(5, 5, 3), vessels={'Submarine':2, 'Destroyer':2, 'Jet':2, 'General':1}):
 
-        self.layer0 = np.ndarray(shape=shape, dtype=float)
-        self.layer0 = self.allocate_pieces(self.layer0, vessels=["Submarine", "General"])
+        self.vessels = vessels
+        self.shape = shape
+        self.game_board = np.ndarray(shape=shape, dtype=float)
+        self.layer0 = np.ndarray(shape=shape[0:2], dtype=float)
+        self.layer0 = self.allocate_pieces(self.layer0, 10, Submarine())
 
-        self.layer1 = np.ndarray(shape=shape, dtype=float)
-        self.layer1 = self.allocate_pieces(self.layer1, vessels=["Destroyer", "General"])
+        self.layer1 = np.ndarray(shape=shape[0:2], dtype=float)
+        self.layer1 = self.allocate_pieces(self.layer1, 12, Destroyer())
 
-        self.layer2 = np.ndarray(shape=shape, dtype=float)
-        self.layer2 = self.allocate_pieces(self.layer2, vessels=["Jet", "General"])
+        self.layer2 = np.ndarray(shape=shape[0:2], dtype=float)
+        self.layer2 = self.allocate_pieces(self.layer2, 4, Jet())
+
+    def layer_initiator(self):
+        layer = np.ndarray(shape=self.shape, dtype=float)
+        layer[:] = np.nan
 
 
     def allocate_pieces(self, layer, number_of_pieces, vessel):
         layer[:] = np.nan
         pieces = [vessel.shape] * number_of_pieces
-        for piece in pieces:
-            piece_dims = piece.shape.shape
+        for piece, n in enumerate(pieces, 1):
+            piece = piece * n
+            piece_dims = piece.shape
             if len(piece_dims) == 1:
-                piece_dims = (piece_dims[0], 1)
+                piece_dims = (1, piece_dims[0])
+                piece = piece.reshape(piece_dims)
 
             for i in range(50):
                 orientation = np.random.randint(0, 2)
                 if orientation == 1:
                     height = piece_dims[1]
                     width = piece_dims[0]
-                    try:
-                        piece.shape = np.rot90(piece.shape, k=-1)
-                    finally:
-                        piece.shape = piece.shape
+                    piece = np.rot90(piece, k=-1)
                 else:
                     height = piece_dims[0]
                     width = piece_dims[1]
-                available_slots = self.empty_places(layer, (height, width))
-                picked_slot = random.choice(available_slots)
-
+                available_slots = empty_places(layer, (height, width))
+                if len(available_slots) != 0:
+                    picked_slot = random.choice(available_slots)
+                    row_idx = picked_slot[0]
+                    column_idx = picked_slot[1]
+                    if row_idx+height > layer.shape[0] or column_idx+width > layer.shape[1]:
+                        continue
+                    else:
+                        layer[row_idx:row_idx+height, column_idx:column_idx+width] = piece
+                        break
+                else:
+                    if i == 49:
+                        raise Exception("Board initialization failed. Try again with less vessels or with a larger board.")
+                    else:
+                        continue
+        return layer
 
     def empty_places(self, layer, piece_dimensions):
-        width = piece_dimensions[0]
-        height = piece_dimensions[1]
+        height = piece_dimensions[0]
+        width = piece_dimensions[1]
         seed_idx = []
         for index, cell_content in np.ndenumerate(layer):
             row_idx = index[0]
@@ -49,8 +69,8 @@ class Board:
                 this_slice = layer[row_idx:row_idx + height, column_idx:column_idx + width]
                 if np.isnan(this_slice).all():
                     seed_idx.append(index)
-
         return seed_idx
+
 
 class Vessel:
     def __init__(self, name, shape, layer, number_of_pieces):
@@ -62,7 +82,7 @@ class Vessel:
 
 class Submarine(Vessel):
     def __init__(self):
-        super().__init__(name="Submarine", shape=np.array([1, 1, 1], dtype=float), layer=0,
+        super().__init__(name="Submarine", shape=np.array([1, 1, 1], dtype=float), layer=[0],
                          number_of_pieces=2)
         self.number_of_pieces = 2
 
@@ -73,7 +93,7 @@ class Submarine(Vessel):
 
 class Destroyer(Vessel):
     def __init__(self):
-        super().__init__(name="Destroyer", shape=np.array([1, 1, 1, 1], dtype=float), layer=1,
+        super().__init__(name="Destroyer", shape=np.array([1, 1, 1, 1], dtype=float), layer=[1],
                          number_of_pieces=2)
         self.number_of_pieces = 2
 
@@ -84,7 +104,7 @@ class Destroyer(Vessel):
 
 class Jet(Vessel):
     def __init__(self):
-        super().__init__(name="Jet", shape=np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0], [0, 1, 0]], dtype=float), layer=2,
+        super().__init__(name="Jet", shape=np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0], [0, 1, 0]], dtype=float), layer=[2],
                          number_of_pieces=2)
         self.number_of_pieces = 2
 
@@ -95,7 +115,7 @@ class Jet(Vessel):
 
 class General(Vessel):
     def __init__(self):
-        super().__init__(name="General", shape=np.array([1], dtype=float), layer=np.random.randint(0, 3),
+        super().__init__(name="General", shape=np.array([1], dtype=float), layer=[np.random.randint(0, 3)],
                          number_of_pieces=1)
 
     @property
@@ -103,9 +123,55 @@ class General(Vessel):
         return bool(np.sum(self.shape) % 1 == 0.5)
 
 
-a = Submarine()
-layer = np.ndarray(shape=(5, 5), dtype=float)
-layer[:] = np.nan
-piece_shape = np.array([1, 1, 1], dtype=float)
-#a = empty_places(layer, piece_shape)
-n = 1
+def allocate_pieces(layer, number_of_pieces, vessel):
+    layer[:] = np.nan
+    pieces = [vessel.shape] * number_of_pieces
+    for piece, n in enumerate(pieces, 1):
+        piece = piece * n
+        piece_dims = piece.shape
+        if len(piece_dims) == 1:
+            piece_dims = (1, piece_dims[0])
+            piece = piece.reshape(piece_dims)
+
+        for i in range(50):
+            orientation = np.random.randint(0, 2)
+            if orientation == 1:
+                height = piece_dims[1]
+                width = piece_dims[0]
+                piece = np.rot90(piece, k=-1)
+            else:
+                height = piece_dims[0]
+                width = piece_dims[1]
+            available_slots = empty_places(layer, (height, width))
+            if len(available_slots) != 0:
+                picked_slot = random.choice(available_slots)
+                row_idx = picked_slot[0]
+                column_idx = picked_slot[1]
+                if row_idx+height > layer.shape[0] or column_idx+width > layer.shape[1]:
+                    continue
+                else:
+                    layer[row_idx:row_idx+height, column_idx:column_idx+width] = piece
+                    break
+            else:
+                if i == 49:
+                    raise Exception("Board initialization failed. Try again with less vessels or with a larger board.")
+                else:
+                    continue
+
+
+def empty_places(layer, piece_dimensions):
+    height = piece_dimensions[0]
+    width = piece_dimensions[1]
+    seed_idx = []
+    for index, cell_content in np.ndenumerate(layer):
+        row_idx = index[0]
+        column_idx = index[1]
+        if row_idx + height <= layer.shape[0] and column_idx + width <= layer.shape[1]:
+            this_slice = layer[row_idx:row_idx + height, column_idx:column_idx + width]
+            if np.isnan(this_slice).all():
+                seed_idx.append(index)
+    return seed_idx
+
+
+a= Board(shape=(11, 11,3))
+g = 2
